a64603b43a36f974dff7fd44fd7afb7a567b047b
David Abramowitz <david.xesllc@gmail.com>
2015-02-13 15:18:44 -0700

    Description StrikeThrough

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -44,7 +44,7 @@ class TasksController < ApplicationController
 
   private
      def task_params
-       params.require(:task).permit(:description)
+       params.require(:task).permit(:description, :checkbox)
      end
 
 
