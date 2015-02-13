873f80f95ef1be6cbf8015df6b30b3a66abec93a
David Abramowitz <david.xesllc@gmail.com>
2015-02-13 13:04:28 -0700

    Partial New & Edit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,7 +12,7 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     if @task.save
-      redirect_to tasks_path, notice: 'Task was successfully created.'
+      redirect_to task_path(@task[:id]), notice: 'Task was successfully created.'
     else
     end
   end
@@ -28,7 +28,7 @@ class TasksController < ApplicationController
   def update
     @task = Task.find(params[:id])
     if @task.update(task_params)
-    redirect_to tasks_path, notice: 'Task was successfully updated.'
+    redirect_to task_path, notice: 'Task was successfully updated.'
     end
   end
 
