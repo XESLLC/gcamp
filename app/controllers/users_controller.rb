fdb82f0a8b0369b7c6fa3a15ee3d55f1ae87be85
David Abramowitz <david.xesllc@gmail.com>
2015-02-19 17:26:38 -0700

    User CRUD

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,21 @@ class UsersController < ApplicationController
 
   end
 
+  def edit
+
+    @user = User.find(params[:id])
+
+  end
+
+  def update
+
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to users_path notice: 'User was successfully updated.'
+    
+  end
+
+
   def show
 
     @user = User.find(params[:id])
