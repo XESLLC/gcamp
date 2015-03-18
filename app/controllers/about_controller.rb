925d7dcc3e3fb884d7cc5e461ec6e221138b878d
David Abramowitz <david.xesllc@gmail.com>
2015-03-17 23:56:00 -0600

    Stats on About Page

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,6 +1,10 @@
 class AboutController < ApplicationController
 
   def about
+    @projects = Project.all
+    @tasks =  Task.all
+    @memberships = Membership.all
+    @comments = Comment.all
   end
 
 end
